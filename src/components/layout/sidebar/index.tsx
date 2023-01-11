368f36d8307795ba02fff00c507b67d0407ba584
Ing-yeo_Developer <sjw7324@naver.com>
2023-01-11 16:16:34 +0900

    feat:render sidebar

--- src/components/layout/sidebar/index.tsx
+++ src/components/layout/sidebar/index.tsx
@@ -1,5 +1,30 @@
+import { BsChevronDoubleLeft } from "@react-icons/all-files/bs/BsChevronDoubleLeft";
+import { BsChevronDoubleRight } from "@react-icons/all-files/bs/BsChevronDoubleRight";
+import { BiExport } from "@react-icons/all-files/bi/BiExport";
+import { BiImport } from "@react-icons/all-files/bi/BiImport";
+import { Inner, Menu, ToggleButton } from "./Style";
+
 const SideBar = () => {
-  return <div></div>;
+  return (
+    <Menu>
+      <ToggleButton type="button" isHidden={true}>
+        <BsChevronDoubleRight data-testid="showButton" size={20} />
+      </ToggleButton>
+      <Inner>
+        <ToggleButton type="button" isHidden={false}>
+          <BsChevronDoubleLeft data-testid="hiddenButton" size={20} />
+        </ToggleButton>
+        <button type="button">
+          <BiExport />
+          Export
+        </button>
+        <button type="button">
+          <BiImport />
+          Import
+        </button>
+      </Inner>
+    </Menu>
+  );
 };
 
 export default SideBar;
