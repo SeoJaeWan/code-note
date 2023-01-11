15dcc2deb59982e1ff00655b91e89fb8d3070673
Ing-yeo_Developer <sjw7324@naver.com>
2023-01-11 17:04:31 +0900

    feat:sidebar toggle

--- src/components/layout/sidebar/index.tsx
+++ src/components/layout/sidebar/index.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { useState } from "react";
 import { BsChevronDoubleLeft } from "@react-icons/all-files/bs/BsChevronDoubleLeft";
 import { BsChevronDoubleRight } from "@react-icons/all-files/bs/BsChevronDoubleRight";
 import { BiExport } from "@react-icons/all-files/bi/BiExport";
@@ -5,14 +7,30 @@ import { BiImport } from "@react-icons/all-files/bi/BiImport";
 import { Inner, Menu, ToggleButton } from "./Style";
 
 const SideBar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggleButton = () => {
+    setToggle((prev) => !prev);
+  };
+
   return (
     <Menu>
-      <ToggleButton type="button" isHidden={true}>
-        <BsChevronDoubleRight data-testid="showButton" size={20} />
+      <ToggleButton
+        type="button"
+        isHidden={!toggle}
+        data-testid="showButton"
+        onClick={handleToggleButton}
+      >
+        <BsChevronDoubleRight size={20} />
       </ToggleButton>
       <Inner>
-        <ToggleButton type="button" isHidden={false}>
-          <BsChevronDoubleLeft data-testid="hiddenButton" size={20} />
+        <ToggleButton
+          type="button"
+          isHidden={toggle}
+          data-testid="hiddenButton"
+          onClick={handleToggleButton}
+        >
+          <BsChevronDoubleLeft size={20} />
         </ToggleButton>
         <button type="button">
           <BiExport />
