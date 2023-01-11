a5e51b7879d71543daa94c7eb307e7e168575680
Ing-yeo_Developer <sjw7324@naver.com>
2023-01-11 16:57:25 +0900

    feat:test Toggle Sider

--- src/components/layout/sidebar/sidebar.test.js
+++ src/components/layout/sidebar/sidebar.test.js
@@ -1,12 +1,28 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import SideBar from ".";
 
 describe("Side Bar", () => {
-  test("has hiddenButton and Export / Import Button", () => {
+  test("has hidden / show Button and Export / Import Button", () => {
     render(<SideBar />);
 
     screen.getByText("Export");
     screen.getByText("Import");
     screen.getByTestId("hiddenButton");
+    screen.getByTestId("showButton");
+  });
+
+  test("toggle sideBar", () => {
+    render(<SideBar />);
+
+    const hiddenButton = screen.getByTestId("hiddenButton");
+    const showButton = screen.getByTestId("showButton");
+
+    expect(hiddenButton).toHaveStyle("display: flex");
+    expect(showButton).toHaveStyle("display: none");
+
+    fireEvent.click(hiddenButton);
+
+    expect(hiddenButton).toHaveStyle("display: none");
+    expect(showButton).toHaveStyle("display: flex");
   });
 });
