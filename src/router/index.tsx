e56088a8d38dafcf4448e3f032235044e975c4a3
Ing-yeo_Developer <52366178+SeoJaeWan@users.noreply.github.com>
2023-01-11 17:26:49 +0900

    Revert "[FEAT] Sidebar Toggle"

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 
-import SideBar from "../components/layout/sidebar";
 import Edit from "../pages/edit";
 import Home from "../pages/home";
 import GlobalStyle from "../styles/global";
@@ -10,10 +9,6 @@ const Router = () => {
   return (
     <BrowserRouter>
       <GlobalStyle />
-
-      <Routes>
-        <Route path="*" element={<SideBar />} />
-      </Routes>
 
       <Routes>
         <Route path="/" element={<Home />} />
